df8e11ded7cc598bae4c422d8c2035c3d2476422
Asim <asim47581@gmail.com>
2021-11-01 15:13:09 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Components/layout/Navbar';
+import Landing from './Components/layout/Landing';
+//import Routes from './components/routing/Routes';
+import Login from "../src/Components/auth/Login";
+import Register from "../src/Components/auth/Register";
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            </div>
+            {/*<Route component={Routes} />*/}
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
